50a8ff799c5b8a39d33e289dc0e2213bfbd3fe5a
Ray <tuhucu97@gmail.com>
2025-06-08 02:20:18 +0900

    feat: train.js Task B

--- train.js
+++ train.js
@@ -1,3 +1,20 @@
+// TASK B
+function countNumbers(str) {
+  let count = 0;
+
+  for (let num of str) {
+    if (num >= '0' && num <= '9') {
+      count++;
+    }
+  }
+
+  return count;
+}
+console.log(countNumbers("1991avgust31")); // ➜ 7
+
+
+
+
 // // TASK A
 // function countLetter(o, notebook) {
 //     let count = 0;
