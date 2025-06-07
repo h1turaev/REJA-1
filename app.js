794f5adf629e3f5bdb7c019dc4df880129ecbe76
Ray <tuhucu97@gmail.com>
2025-06-08 02:06:50 +0900

    feat:CRUD Operatsialari

--- app.js
+++ app.js
@@ -51,9 +51,18 @@ app.set("view engine", "ejs");   // EJS da front-endni yasaymiz back-end ichida.
 
 // 4. Routing code
 app.post("/create-item", (req, res) => {
-    console.log(req.body);
-res.json({test:"success"});
+    console.log("user entered /create-item");
+    const new_reja = req.body.reja;
+    db.collection("plans").insertOne({ reja: new_reja },  (err,data) => {
+        if (err) {
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            res.end("Successfully added");
+        }
+    });
 });
+
 
 app.get("/author", (req, res) => {
     res.render("author", {user: user});
@@ -61,7 +70,17 @@ app.get("/author", (req, res) => {
 
 
 app.get('/', function (req, res) {
-    res.render("reja");
+    console.log("user entered /")
+    db.collection("plans")
+    .find()
+    .toArray((err, data) => {
+        if(err) {
+            console.log(err);
+            res.end("Something went wrong");
+        } else {
+            res.render("reja", { items: data });
+        }
+    });
 });
 
 module.exports = app;
