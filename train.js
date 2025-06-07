0579e8dda4f5820fd74f46833460b720b3f04689
Ray <tuhucu97@gmail.com>
2025-06-07 23:36:12 +0900

    fix: refactoring

--- train.js
+++ train.js
@@ -12,46 +12,47 @@
 // console.log(countLetter("o", "notebook"));  // Natija: 3
 
 
-// NODE JS EVENT LOOP VA CALLBACK TUSHUNCHALARI
+// // NODE JS EVENT LOOP VA CALLBACK TUSHUNCHALARI
 
-// console.log("Jack Ma maslahatlari");
-// const list = [
-//     "yaxshi talab bo'ling", //0-20
-//     "to'gri boshliq tanlang va ko'proq xato qiling", //20-30
-//     "o'zingizga ishlashni boshlang", //30-40
-//     "siz kuchli bo'lgan narsalar ustida ishlang", //40-50
-//     "yoshlarga invsetitsiya qiling", //50-60
-//     "endi yoshlarga maslahat bering", //60
-// ];
+// // console.log("Jack Ma maslahatlari");
+// // const list = [
+// //     "yaxshi talab bo'ling", //0-20
+// //     "to'gri boshliq tanlang va ko'proq xato qiling", //20-30
+// //     "o'zingizga ishlashni boshlang", //30-40
+// //     "siz kuchli bo'lgan narsalar ustida ishlang", //40-50
+// //     "yoshlarga invsetitsiya qiling", //50-60
+// //     "endi yoshlarga maslahat bering", //60
+// // ];
 
-// function maslahatBering(a, callback) {
-//     if (typeof a !=="number") callback("insert number please", null);
-//     else if (a <= 20) callback(null, list[0]);
-//     else if (a > 20 && a <= 30) callback(null, list[1]);
-//     else if (a > 30 && a <= 40) callback(null, list[2]);
-//     else if (a > 40 && a <= 50) callback(null, list[3]);
-//     else if (a > 50 && a <= 60) callback(null, list[4]);
-//     else {
-//         setInterval(function () {
-//             callback(null, list[5]);
-//         }, 1000);
-//     }
-// }
+// // function maslahatBering(a, callback) {
+// //     if (typeof a !=="number") callback("insert number please", null);
+// //     else if (a <= 20) callback(null, list[0]);
+// //     else if (a > 20 && a <= 30) callback(null, list[1]);
+// //     else if (a > 30 && a <= 40) callback(null, list[2]);
+// //     else if (a > 40 && a <= 50) callback(null, list[3]);
+// //     else if (a > 50 && a <= 60) callback(null, list[4]);
+// //     else {
+// //         setInterval(function () {
+// //             callback(null, list[5]);
+// //         }, 1000);
+// //     }
+// // }
 
-// console.log("passed here 0");
-// maslahatBering(65, (err, data) => {
-//     if(err) console.log('ERROR CHIQDI!!!', err);
-//     else {
-//     console.log('javob:', data);
-// }
-// });
-// console.log("passed here 1");
+// // console.log("passed here 0");
+// // maslahatBering(65, (err, data) => {
+// //     if(err) console.log('ERROR CHIQDI!!!', err);
+// //     else {
+// //     console.log('javob:', data);
+// // }
+// // });
+// // console.log("passed here 1");
 
 
 
 // // ASYNCHRONOUS FUNCTIONLARNI QO'LLASH
 
 // console.log("Jack Ma maslahatlari");
+
 // const list = [
 //     "yaxshi talab bo'ling", //0-20
 //     "to'gri boshliq tanlang va ko'proq xato qiling", //20-30
@@ -82,7 +83,7 @@
 // }
 
 
-// then/catch
+// // then/catch
 // console.log("passed here 0");
 // maslahatBering(65) 
 // .then((data) => {
@@ -95,7 +96,7 @@
 
 
 
-// async/await
+// // async/await
 // async function run() {
 //     let javob = await maslahatBering(22);
 //     console.log(javob);
@@ -111,3 +112,21 @@
 
 
 
+// console.log("===== EXECUTE =====");
+
+// // DEFINE
+// function qoshish(a, b, callback) {
+//   if (b === 10) {
+//     callback("bizga 20 soni kerak!", null);
+//   } else {
+//     callback(null, a + b);
+//   }
+// }
+
+// // CALL
+// qoshish(7, 10, (err, data) => {
+//   if (err) console.log("Error:", err);
+//   else {
+//     console.log("data:", data);
+//   }
+// });
